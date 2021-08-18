aee5f3a3667834d191e53ad5ee97bd20ee9a077e
Diane Nzeutem <deedeespy@gmail.com>
2021-08-19 01:14:40 +0200

    swipe is ok !

--- Instagrid/Controler/ViewController.swift
+++ Instagrid/Controler/ViewController.swift
@@ -4,109 +4,102 @@
 //
 //  Created by Dee Dee on 11/08/2021.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
     private var bntImage : UIButton!
-    // Swipe gesture
-    private let gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeFunc(gesture:)))
-    // Frame contening the images
-    @IBOutlet weak var frameview: FrameView!
+        // Swipe gesture
+         private var gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeFunc(gesture:)))
+        // Frame contening the images
+    @IBOutlet weak var frameView: FrameView!
     // 3 buttons for layout selection
-    @IBOutlet weak var btnLayout1: UIButton!
-    @IBOutlet weak var btnLayout2: UIButton!
-    @IBOutlet weak var btnLayout3: UIButton!
     
+    @IBOutlet weak var btnLayout1: UIButton!
+    
+    @IBOutlet weak var btnLayout2: UIButton!
+    
+    @IBOutlet weak var btnLayout3: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeFunc(gesture:)))
+    }
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+        gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeFunc(gesture:)))
         self.view.addGestureRecognizer(gesture)
-        // checking device orientation
-        
-//        let size = UIScreen.main.bounds.size
-//        if size.width < size.height {
-//            gesture.direction = .up
-//        } else if size.width > size.height {
-//            gesture.direction = .left
-//        }
-//        if UIDevice.current.orientation.isLandscape {
-//            gesture.direction = .left
-//        } else {
-//            gesture.direction = .up
-//        }
-    }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        // checking device orientation
-        if UIDevice.current.orientation.isLandscape {
-            gesture.direction = .left
-        } else {
-            gesture.direction = .up
+            determineMyDeviceOrientation()
         }
-//        let size = UIScreen.main.bounds.size
-//        if size.width < size.height {
-//            gesture.direction = .up
-//        } else if size.width > size.height {
-//            gesture.direction = .left
-//        }
-    }
-     // change frameView layout
+        
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+           
+           determineMyDeviceOrientation()
+       }
+    
+        func determineMyDeviceOrientation()
+        {
+            if UIDevice.current.orientation.isLandscape {
+                gesture.direction = .left
+            } else {
+                gesture.direction = .up
+            }
+        }
+    
+    // change frameView layout
     @IBAction func setLayout1(_ sender: UIButton) {
-        frameview.style = .layout1
-        resetSelection(sender)
-    }
+        frameView.style = .layout1
+                resetSelection(sender)
+}
     
     @IBAction func setLayout2(_ sender: UIButton) {
-        frameview.style = .layout2
-        resetSelection(sender)
-    }
+        frameView.style = .layout2
+                resetSelection(sender)
+}
     
     @IBAction func setLayout3(_ sender: UIButton) {
-        frameview.style = .layout3
-        resetSelection(sender)
-    }
+        frameView.style = .layout3
+                resetSelection(sender)
+}
     // change image of UIButton
     @IBAction func changeImage(_ sender: UIButton) {
         bntImage = sender
-        openImagePicker()
+                openImagePicker()
     }
+    
     // deselect every button except sender
-    private func resetSelection(_ sender : UIButton){
-        btnLayout1.isSelected = false
-        btnLayout2.isSelected = false
-        btnLayout3.isSelected = false
-        
-        sender.isSelected = true
-    }
-    private func animView() {
-        let screenHeight = UIScreen.main.bounds.height
-        let screenWidth = UIScreen.main.bounds.width
-        var translationTransform: CGAffineTransform
-        if UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight {
-            translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
-        } else {
-            translationTransform = CGAffineTransform(translationX: 0, y: -screenHeight)
+        private func resetSelection(_ sender : UIButton){
+            btnLayout1.isSelected = false
+            btnLayout2.isSelected = false
+            btnLayout3.isSelected = false
+            
+            sender.isSelected = true
         }
-        UIView.animate(withDuration: 0.4) { self.frameview.transform = translationTransform }
-    }
-    
-    private func shareImage(){
-        let renderer = UIGraphicsImageRenderer(size: frameview.bounds.size)
-        let screenshot = renderer.image { _ in
-            frameview.drawHierarchy(in: frameview.bounds, afterScreenUpdates: true)
+        private func animView() {
+            let screenHeight = UIScreen.main.bounds.height
+            let screenWidth = UIScreen.main.bounds.width
+            var translationTransform: CGAffineTransform
+            if UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight {
+                translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
+            } else {
+                translationTransform = CGAffineTransform(translationX: 0, y: -screenHeight)
+            }
+            UIView.animate(withDuration: 0.4) { self.frameView.transform = translationTransform }
         }
-        let activityControler = UIActivityViewController(activityItems: [screenshot], applicationActivities: nil)
-        present(activityControler, animated: true, completion: nil)
-    }
-    // Openning share view on swipe
-    @objc func swipeFunc(gesture: UISwipeGestureRecognizer){
-        animView()
-        shareImage()
         
-    }
-    
+        private func shareImage(){
+            let renderer = UIGraphicsImageRenderer(size: frameView.bounds.size)
+            let screenshot = renderer.image { _ in
+                frameView.drawHierarchy(in: frameView.bounds, afterScreenUpdates: true)
+            }
+            let activityControler = UIActivityViewController(activityItems: [screenshot], applicationActivities: nil)
+            present(activityControler, animated: true, completion: nil)
+        }
+        // Openning share view on swipe
+        @objc func swipeFunc(gesture: UISwipeGestureRecognizer){
+            animView()
+            shareImage()
+            
+        }
+
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -126,6 +119,5 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         controler.allowsEditing = true
         self.present(controler, animated: true)
     }
-    
-}
 
+}
--- Instagrid/View/FrameView.swift
+++ Instagrid/View/FrameView.swift
@@ -2,41 +2,41 @@
 //  FrameView.swift
 //  Instagrid
 //
-//  Created by Dee Dee on 11/08/2021.
+//  Created by Dee Dee on 19/08/2021.
 //
 
 import UIKit
 
-class FrameView: UIStackView {
+class FrameView: UIView {
+
     // Outlet for the buttons
-    @IBOutlet private var button1 : UIButton!
-    @IBOutlet private var button2 : UIButton!
-    @IBOutlet private var button3 : UIButton!
-    @IBOutlet private var button4 : UIButton!
-    // Enumeration of the different styles
-    enum Style {
-        case layout1, layout2, layout3
-        
-    }
-    
-    var style : Style = .layout2 {
-        didSet {
-            setStyle(style)
-        }
-    }
-    // Changing frameView layout
-    private func setStyle (_ style : Style){
-        switch style {
-        case .layout1:
-            button2.isHidden = true
-            button4.isHidden = false
-        case .layout2:
-            button4.isHidden = true
-            button2.isHidden = false
-        case .layout3 :
-            button4.isHidden = false
-            button2.isHidden = false
+        @IBOutlet private var button1 : UIButton!
+        @IBOutlet private var button2 : UIButton!
+        @IBOutlet private var button3 : UIButton!
+        @IBOutlet private var button4 : UIButton!
+        // Enumeration of the different styles
+        enum Style {
+            case layout1, layout2, layout3
             
         }
-    }
+        
+        var style : Style = .layout2 {
+            didSet {
+                setStyle(style)
+            }
+        }
+        // Changing frameView layout
+        private func setStyle (_ style : Style){
+            switch style {
+            case .layout1:
+                button2.isHidden = true
+                button4.isHidden = false
+            case .layout2:
+                button4.isHidden = true
+                button2.isHidden = false
+            case .layout3 :
+                button4.isHidden = false
+                button2.isHidden = false
+            }
+        }
 }
